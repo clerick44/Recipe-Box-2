bbe9bf01e7ebdc96a000c08b3116cc63c6e899a6
Jessse Erickson <jerickson@2001-48F8-7028-80-0-0-0-FFEB-dynamic.midco.net>
2022-06-23 12:08:00 -0500

    recipe post route passes

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -1,8 +1,36 @@
 const express = require("express");
+const Recipe = require("../models/recipeModel");
 
 const router = express.Router();
 
+// GET all recipes
+router.get("/", async (req, res) => {
+  // const { title, ingredients, steps } = req.body;
+  res.json({ mssg: "GET all recipe" });
+  // try {
+  //   const workout = await Workout.create({ title, ingredients, steps });
+  //   res.status(200).json(workout)
+  // } catch (error) {
+  //   res.status(400).json({error: error.message})
+  // }
+});
 
+// GET single recipe
 
+// POST new recipe
+router.post("/", async (req, res) => {
+  const { title, ingredients, steps } = req.body;
 
-module.exports = router
+  try {
+    const recipe = await Recipe.create({ title, ingredients, steps });
+    res.status(200).json(recipe);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+//DELETE recipe
+
+//UPDATE recipe
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -9,18 +9,22 @@ const recipeRoutes = require("./routes/recipes.js");
 const app = express();
 
 //middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
+// routes
+app.use("/api/recipes", recipeRoutes);
 //conect to db
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     //listen for requests
     app.listen(process.env.PORT, () => {
-      console.log("Listening on port 4000");
+      console.log("Connected to db and Listening on port", process.env.PORT);
     });
   })
   .catch((error) => {
